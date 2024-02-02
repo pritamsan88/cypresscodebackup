2c1dfb9b6feeb0059d35a92aa48a403931171584
Pritam Sanyal <sanyal.pritam358@gmail.com>
2024-02-02 19:07:02 +0530

    new modified code is added to ardea file

--- cypress/e2e/ardea.cy.js
+++ cypress/e2e/ardea.cy.js
@@ -15,7 +15,25 @@ describe('search functionality test for ardea',()=>{
       cy.xpath('/html/body/div[1]/sticky-header/div/nav/div/div[2]/ul/li[1]/form/input').type('Amalfi - Dinner Set For 6',{force: true})
       cy.wait(2000)
       cy.xpath('/html/body/div[1]/sticky-header/div/nav/div/div[2]/ul/li[1]/form/a[1]/input').click({force: true})
+      cy.wait(2000)
+      cy.xpath('/html/body/main/section/div/div[1]/div/predictive-search/main-search/form/div/input[1]').clear()
+      cy.wait(2000)
+      cy.xpath('/html/body/main/section/div/div[1]/div/predictive-search/main-search/form/div/input[1]').type("dinner for 6")
+      cy.get("li[role='option']").each(function($ele,index,$list){
+       if($ele.text().includes("Verde Butterfly - 14 Pieces Dinner Set for 6")){
 
+       cy.wrap($ele).click()
+
+       }
+       else
+       {
+         cy.log($ele.text())
+
+       }
+
+
+      })
+    
     })
 
 })
